cdf16dfb2f2b9792a0d5c8d326ffec244485be6b
Lucilorate <lucilorate@proton.me>
2025-10-31 03:51:29 +0100

    Auto stash before merge of "main" and "origin/main"

--- web-interface/src/App.tsx
+++ web-interface/src/App.tsx
@@ -3,7 +3,27 @@ import Interface from "@/components/Interface.tsx";
 function App() {
 
     return (
-        <Interface/>
+        <div className={"h-screen w-screen p-4 bg-background"}>
+            <div className={"container mx-auto space-y-4"}>
+                <Header/>
+                <div className={"grid grid-cols-3 gap-4"}>
+                    {/*left column*/}
+                    <div className={"col-span-2 space-y-4"}>
+                        <VideoFeed/>
+                    </div>
+
+                    {/*right column*/}
+                    <div className={"space-y-4 flex flex-col "}>
+                        <InfoPanel/>
+                        <Terminal/>
+                    </div>
+
+                    <div className={"col-span-3"}>
+                        <Control/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
